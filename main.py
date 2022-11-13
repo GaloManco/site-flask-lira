714ed321db22ebbb147adde50473bcbe172d7e6c
unknown <minhacontajonas@gmail.com>
2022-11-12 21:52:30 -0300

    Instalado Flask criado diretório templates e editado links para abas index.html, erro.html, userlogin.html e diretório css.

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Página main
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/usuarios/<string:name>')
+def usuarioUser(name):
+    return render_template('userLogin.html', userLogin=name)
+
+# Página erro
+@app.route('/<string:name>')
+def erro(name):
+    return render_template('erro.html', variavelErro=name)
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
